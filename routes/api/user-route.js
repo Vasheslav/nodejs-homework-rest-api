534480ab4b942e503c11ebbc9cb66ff9d4228b75
Viasheslav Rashnik <vasheslav1000@gmail.com>
2023-08-12 01:06:23 +0300

    hw05-avatars

--- routes/api/user-route.js
+++ routes/api/user-route.js
@@ -7,6 +7,7 @@ const {
   login,
   current,
   logout,
+  updateAvatar,
 } = require("../../controllers/userController");
 
 const { userAddSchema, userLoginSchema } = require("../../shemas/user-shema");
@@ -15,10 +16,18 @@ const { validateData } = require("../../decorators/contactValidations");
 
 const { authenticate } = require("../../middlewares/authenticate");
 
+const { upload } = require("../../middlewares/apload");
+
 authRouter.post("/register", validateData(userAddSchema), register);
 authRouter.post("/login", validateData(userLoginSchema), login);
 authRouter.get("/current", authenticate, current);
 authRouter.post("/logout", authenticate, logout);
+authRouter.patch(
+  "/avatars",
+  upload.single("avatarURL"),
+  authenticate,
+  updateAvatar,
+);
 
 module.exports = {
   authRouter,
